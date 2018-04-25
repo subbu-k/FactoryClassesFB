d1c9c60c0874a4305d46d74ebb7c97b3a315c414
Subramanyam <subbu377.k@gmail.com>
2018-04-25 12:06:03 -0700

    creating Factory class by implementing FactoryBean(I)

---
+++ FactoryClassesFB/src/beans/Audi.java
@@ -0,0 +1,7 @@
+package beans;
+
+public class Audi implements Car {
+public void drive() {
+	System.out.println("Audi is  speed Limit is 100");
+}
+}
---
+++ FactoryClassesFB/src/beans/CarFactory.java
@@ -0,0 +1,31 @@
+
+package beans;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class CarFactory implements FactoryBean {
+	private String carname;
+	
+		public void setCarname(String carname) {
+		this.carname = carname;
+	}
+
+		public Object getObject()throws Exception  {
+			Car c=(Car)Class.forName(carname).newInstance();
+			return c;
+			
+		}
+
+		@Override
+		public Class getObjectType() {
+			// TODO Auto-generated method stub
+			return Car.class;
+		}
+
+		@Override
+		public boolean isSingleton() {
+			// TODO Auto-generated method stub
+			return true;
+		}
+	
+}
